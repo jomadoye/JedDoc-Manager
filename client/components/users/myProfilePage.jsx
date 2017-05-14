d5e42c4bff7ca3a4871b79186ea178d8abb033f3
Jedidiah Omadoye <jedidiahomadoye@Andelas-MacBook-Pro.local>
2017-05-15 00:14:30 +0100

    feature(client) fix very annoying access propsTypes deprecated warning on browser console
    [143585203]

--- client/components/users/myProfilePage.jsx
+++ client/components/users/myProfilePage.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as userAction from '../../actions/userAction';
 import * as loginAction from '../../actions/loginActions';
