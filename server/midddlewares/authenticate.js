21d8fab640283631e425825ab331ed9933da07d9
Omadoye Jedidiah <jedidiah.omadoye@andela.com>
2017-05-26 18:45:00 +0100

    feature(client) implement middleware seperation feedback

--- server/midddlewares/authenticate.js
+++ server/midddlewares/authenticate.js
@@ -1,53 +1,6 @@
-import jwt from 'jsonwebtoken';
 import models from '../models';
 
-const secret = process.env.SECRET;
-const User = models.Users;
-
 export default {
-  verifyToken(req, res, next) {
-    let token;
-    if (req.headers.authorization) {
-      token = req.headers.authorization.split(' ')[1];
-    } else if (req.headers['x-access-token']) {
-      token = req.headers['x-access-token'];
-    }
-    if (token) {
-      jwt.verify(token, secret, (err, decoded) => {
-        if (err) {
-          return res.status(403)
-            .send({
-              success: false,
-              message: 'Incorrect token.',
-            });
-        }
-        User.findById(decoded.data.id)
-          .then((user) => {
-            if (!user) {
-              return res.status(403)
-                .send({
-                  success: false,
-                  message: 'This user does not exist',
-                });
-            }
-            req.decoded = decoded;
-            return next();
-          })
-          .catch(error => res.status(400)
-            .send({
-              success: false,
-              message: 'Error finding current users',
-              error,
-            }));
-      });
-    } else {
-      return res.status(403)
-        .send({
-          success: false,
-          message: 'No token provided.',
-        });
-    }
-  },
 
   isAdmin(req, res, next) {
     models.Roles.findById(req.decoded.data.roleId)
@@ -57,7 +10,7 @@ export default {
         }
         return res.status(403)
           .send({
-            success: false,
+            
             message: 'Admin access is required',
           });
       })
@@ -65,7 +18,7 @@ export default {
         .send(error));
   },
 
-  isAdminOrOwner(req, res, next) {
+  isOwner(req, res, next) {
     const loggedInUserId = req.decoded.data.id;
     const loggedInUserRoleId = req.decoded.data.roleId;
     const documentId = req.params.documentId;
@@ -82,7 +35,7 @@ export default {
           }
           return res.status(403)
             .json({
-              success: false,
+              
               message: 'unauthorized to perform this request',
             });
         })
@@ -92,7 +45,7 @@ export default {
     }
     return res.status(403)
       .json({
-        success: false,
+        
         message: 'unauthorized to perform this request',
       });
   },
