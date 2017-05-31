a7b0f76fd1dcb85647ab4d834669fe3401670afd
Omadoye Jedidiah <jedidiah.omadoye@andela.com>
2017-05-31 23:33:04 +0100

    feature(server) add test for middleware
    [feedback]

--- server/test/middlewares/authenticate.spec.js
+++ server/test/middlewares/authenticate.spec.js
@@ -1,22 +1,78 @@
-// import decode from 'jwt-decode';
-// import jwt from 'jsonwebtoken';
-// import chai from 'chai';
-// import Authenticate from '../../midddlewares/authenticate';
-// import userController from'../../controllers/users';
-// import helperUsers from '../../test-utility/helpers/api/helperUsers';
+import chai from 'chai';
+import httpMocks from 'node-mocks-http';
+import events from 'events';
+import chaiHttp from 'chai-http';
+import server from '../../app';
+import authenticate from '../../midddlewares/authenticate';
+import sampleUsers from '../../test-utility/helpers/api/helperUsers';
 
-// const mockReq = {
-//   body: helperUsers.middlewareTestUser,
-// };
+const expect = chai.expect;
 
-// const createUserReq = userController.create(mockReq);
-// console.log(createUserReq)
+const middlewareTestUser = sampleUsers.middlewareTestUser;
+const responseEvent = () => httpMocks
+.createResponse({ eventEmitter: events.EventEmitter });
+chai.use(chaiHttp);
 
-// describe('Authenticate middleware', () => {
-//   describe('Verifies Token', () => {
-//     it('should verify token  for valid user', () => {
-//       expect(Authenticate.verifyToken(userToken))
-//         .to.be.user;
-//     });
-//   });
-// });
+describe('verifyToken', () => {
+  let token;
+  before((done) => {
+    chai.request(server)
+      .post('/users')
+      .send(middlewareTestUser)
+      .end((err, res) => {
+        token = res.body.token;
+        done();
+      });
+  });
+
+  describe('verifyToken', () => {
+    it('should verify token', (done) => {
+      const response = responseEvent();
+
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/users',
+        headers: { 'x-access-token': token },
+      });
+
+      const callback = () => {
+        expect(request.decoded.data.roleId).to.equal(1);
+        done();
+      };
+      authenticate.verifyToken(request, response, callback);
+    });
+    it('should catch Incorrect token', (done) => {
+      const response = responseEvent();
+
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/users',
+        headers: { 'x-access-token': `faketoken${token}` },
+      });
+
+      const callback = () => {};
+      authenticate.verifyToken(request, response, callback);
+      response.on('end', () => {
+        expect(response._getData().message).to
+        .equal('Incorrect token.');
+      });
+      done();
+    });
+    it('should catch empty token', (done) => {
+      const response = responseEvent();
+
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/users',
+      });
+
+      const callback = () => {};
+      authenticate.verifyToken(request, response, callback);
+      response.on('end', () => {
+        expect(response._getData().message).to
+        .equal('No token provided.');
+      });
+      done();
+    });
+  });
+});
